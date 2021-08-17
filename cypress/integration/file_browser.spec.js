efcfd69a4a7503d3d508ce1ea91e71c35f7d67d9
choiseunghyeon <ket8780@gmail.com>
2021-08-17 22:51:37 +0900

    added test case of moving directory

--- cypress/integration/file_browser.spec.js
+++ cypress/integration/file_browser.spec.js
@@ -5,26 +5,36 @@ const initialState = [
   { type: "file", name: "testFile" },
 ];
 
+const nextState = [
+  { type: "dir", name: "history" },
+  { type: "dir", name: "nextFolder" },
+  { type: "file", name: "nextFile" },
+];
+
 const itemSelector = "[data-testid=item]";
 const pathSelector = "[data-testid=pathSpan]";
 const pathArrowSelector = "[data-testid=pathNavigation]";
 const treeItemSelector = "[data-testid=treeItem]";
 
 const initialPath = "C:/";
+const nextPath = "C:/Data";
+const BASE_URL = "http://localhost:5000";
 
-describe("render file-browser", () => {
-  beforeEach(() => {
-    // cy.server();
-    // cy.route("/", initialState);
-    cy.intercept("GET", `http://localhost:5000/all?path=${initialPath}`, {
-      body: initialState,
-      //path=C://Bitnami HTTP/1.1
-      //   query: {
-      //     path: "C:/",
-      //   },
-    });
+beforeEach(() => {
+  cy.intercept("GET", `${BASE_URL}/all?path=${initialPath}`, {
+    body: initialState,
+    //path=C://Bitnami HTTP/1.1
+    //   query: {
+    //     path: "C:/",
+    //   },
   });
 
+  cy.intercept("GET", `${BASE_URL}/all?path=${nextPath}`, {
+    body: nextState,
+  });
+});
+
+describe("render file-browser", () => {
   it("render correctly", () => {
     cy.visit("/");
 
@@ -36,9 +46,6 @@ describe("render file-browser", () => {
 
       expect(items[1]).to.have.contain("폴더");
       expect(items[1]).to.have.contain("Image");
-
-      expect(items[2]).to.have.contain("폴더");
-      expect(items[2]).to.have.contain("Setup");
 
       expect(items[3]).to.have.contain("파일");
       expect(items[3]).to.have.contain("testFile");
@@ -62,4 +69,48 @@ describe("render file-browser", () => {
       expect(items[3]).to.have.contain("Setup");
     });
   });
+});
+
+describe("when item dblclick then move next path correctly", () => {
+  it("when mainbody item dblclick then move next path", () => {
+    /**
+     * navigation, tree, mainBody에서 folder 이동 기능 테스트
+     */
+    cy.visit("/");
+
+    cy.get(itemSelector).contains("Data").dblclick();
+
+    cy.get(itemSelector).within(items => {
+      expect(items).to.have.length(nextState.length);
+      expect(items[0]).to.have.contain("폴더");
+      expect(items[0]).to.have.contain("history");
+
+      expect(items[1]).to.have.contain("폴더");
+      expect(items[1]).to.have.contain("nextFolder");
+
+      expect(items[2]).to.have.contain("파일");
+      expect(items[2]).to.have.contain("nextFile");
+    });
+  });
+
+  it("when tree item dblclick then move next path", () => {
+    cy.visit("/");
+
+    cy.get(treeItemSelector).contains("C:/").click();
+
+    cy.get(treeItemSelector).contains("Data").click();
+
+    cy.get(itemSelector).within(items => {
+      expect(items).to.have.length(nextState.length);
+      expect(items[0]).to.have.contain("폴더");
+      expect(items[0]).to.have.contain("history");
+
+      expect(items[1]).to.have.contain("폴더");
+      expect(items[1]).to.have.contain("nextFolder");
+
+      expect(items[2]).to.have.contain("파일");
+      expect(items[2]).to.have.contain("nextFile");
+    });
+  });
+  // navigation에서 layer 나오는거 이동 테스트 하기
 });
